512452b312738bcbf071dcee52a716556af760c2
adnane <adnane.amari@hotmail.com>
2023-07-26 10:13:20 +0200

    aa

---
+++ public/build/back_scripts.js
@@ -0,0 +1,18 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["back_scripts"],{
+
+/***/ "./assets/back/scripts/main.ts":
+/*!*************************************!*\
+  !*** ./assets/back/scripts/main.ts ***!
+  \*************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/back/scripts/main.ts"));
+/******/ }
+]);
---
+++ public/build/front_scripts.js
@@ -0,0 +1,18 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["front_scripts"],{
+
+/***/ "./assets/front/scripts/main.ts":
+/*!**************************************!*\
+  !*** ./assets/front/scripts/main.ts ***!
+  \**************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/front/scripts/main.ts"));
+/******/ }
+]);
