bf335e03fdf7ce58e02aec19b10c0131dc45b7af
adnane <adnane.amari@hotmail.com>
2023-07-26 12:11:56 +0200

    form

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Listing;
+use App\Form\AddListingType;
+use App\Repository\ListingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
+
+
+#[Route('/listing')]
 class ListingController extends AbstractController
 {
-    #[Route('/listing', name: 'app_listing')]
-    public function index(): Response
+
+    private $translator;
+
+    #[Route('/', name: 'app_listing')]
+    public function index(ListingRepository $listingRepository): array
     {
-        return $this->render('listing/index.html.twig', [
+
+        return $listingRepository->findAll('listing/index.html.twig', [
             'controller_name' => 'ListingController',
+        ]);
+
+    }
+
+    #[Route('/add', name: 'add', methods: ['GET', 'POST'])]
+    public function add(Request $request, ListingRepository $listingRepository): Response
+    {
+        $listing = new Listing();
+        $form = $this->createForm(AddListingType::class, $listing);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $listingRepository->save($listing, true);
+
+            return $this->redirectToRoute('app_listing', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('listing/index.html.twig', [
+            'listing' => $listing,
+            'form' => $form,
         ]);
     }
 }
+
+
--- src/Repository/ListingRepository.php
+++ src/Repository/ListingRepository.php
@@ -45,4 +45,5 @@ class ListingRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+
 }
